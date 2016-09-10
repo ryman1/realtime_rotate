97903dfc5186bffeb2df734d0eefadc60fd99eb2
ryman1 <ryman1@gmail.com>
2016-09-10 16:13:44 -0400

    first successful test run completed
    changed front/back logic to check for picture number eveness

--- realtimerotate.py
+++ realtimerotate.py
@@ -11,10 +11,18 @@ if __name__ == '__main__':
     # Create a watchdog observer that updates the file path of the image to be modified in real time.
     class MyFileSystemEventHandler(FileSystemEventHandler):
         def on_modified(self, event):
-            if re.search(r'\.jpg$', event.src_path):
-                global activejpg
-                print('Active file changed to ' + event.src_path)
-                activejpg = event.src_path
+            # If the picture number is even, it's a front
+            try:
+                if int(re.search(r'(.)\.jpg$', event.src_path).group(1)) % 2 == 0:
+                    global activejpgfront
+                    print('Active file front changed to ' + event.src_path)
+                    activejpgfront = event.src_path
+                if not int(re.search(r'(.)\.jpg$', event.src_path).group(1)) % 2 == 0:
+                    global activejpgback
+                    print('Active file back changed to ' + event.src_path)
+                    activejpgback = event.src_path
+            except AttributeError:
+                pass
 
     def kbfunc():
         x = msvcrt.kbhit()
@@ -29,11 +37,18 @@ if __name__ == '__main__':
         # Pause the observer until all write operations are completed
         observer.unschedule(scheduledwatch)
         try:
-            im1 = Image.open(activejpg)
+            # Rotate front photo
+            im1 = Image.open(activejpgfront)
             im2 = im1.rotate(degrees, expand=True)
             im1.close()
-            im2.save(activejpg)
+            im2.save(activejpgfront)
+            # Rotate back photo
+            im1 = Image.open(activejpgback)
+            im2 = im1.rotate(degrees * -1, expand=True)
+            im1.close()
+            im2.save(activejpgback)
             im2.close()
+        # For when no photo has been selected yet
         except AttributeError:
             pass
         # Unpause the observer
@@ -43,7 +58,8 @@ if __name__ == '__main__':
         operatingpath = sys.argv[1]
     except IndexError:
         operatingpath = os.getcwd()
-    activejpg = None
+    activejpgfront = None
+    activejpgback = None
     observer = Observer()
     handler = MyFileSystemEventHandler()
     scheduledwatch = observer.schedule(handler, operatingpath, recursive=True)
@@ -70,7 +86,7 @@ if __name__ == '__main__':
                         rotationdegrees = 180
                     rotate(rotationdegrees)
                     try:
-                        print('rotated ' + activejpg + ' ' + direction)
+                        print('rotated ' + activejpgfront + ' ' + direction)
                     except TypeError:
                         print('no photo selected yet')
             time.sleep(.01)
